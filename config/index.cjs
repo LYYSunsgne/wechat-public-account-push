dfb2ecb3c60c88266e53299dfe0c0400c6cd33aa
LYYSunsgne <81435395+LYYSunsgne@users.noreply.github.com>
2022-12-01 17:53:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,14 +16,10 @@ const USER_CONFIG = {
 
   USERS: [
     {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      name: '宝贝',      // 想要发送的人的名字
+      oL7K45sfvtfKFVxP9Nwg88X2NTUw: '',      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+	    3Yv1U2umvRHkmys7aKOD4noA1AN0RVRnU9KuxQ_nfr0: '',      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      horoscopeDate: '12-1',      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -40,7 +36,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-09-26' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -49,11 +45,11 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  	3Yv1U2umvRHkmys7aKOD4noA1AN0RVRnU9KuxQ_nfr0: '',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'oL7K45sfvtfKFVxP9Nwg88X2NTUw',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
